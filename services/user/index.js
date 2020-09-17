e50635c585086827294fc1418458c4ce6e1367c2
Hammad Ali <63840111+HammadAli-WD@users.noreply.github.com>
2020-09-17 23:03:03 +0200

    routes authorization for user

--- services/user/index.js
+++ services/user/index.js
@@ -3,13 +3,13 @@ const UserModel = require("./schema")
 const multer = require("multer");
 const fs = require("fs-extra");
 const path = require("path");
+const { authorize, authenticate } = require("passport");
 const upload = multer({});
 
-router.get("/", async (req, res, next) => {
+router.get("/", authorize, async (req, res, next) => {
     try{
         const users = await UserModel.find();
-        if (users.length < 1) {
-            console.log("user not found")
+        if (users.length < 1) {            
             res.status(200).send("User List is Empty")            
         } else {
             console.log(users);
@@ -17,6 +17,14 @@ router.get("/", async (req, res, next) => {
         }          
     } catch (error) {
         next()
+    }
+})
+
+router.get("/me", authorize, (req, res, next) => {
+    try {
+        res.send(req.user)
+    } catch (error) {
+        next(error)
     }
 })
 
@@ -29,7 +37,44 @@ router.post("/Register", async (req, res, next) => {
         next(error);
     }
 })
+ router.post("/signIn", async(req, res, next)=>{
+     try {
+         const { email, password} = req.body;
+         const user = await UserModel.findByCredentials(email, password);
+         const { token, refreshToken } = await authenticate(user);
+         res.cookie("accessToken", token, {
+            httpOnly: true,
+            path: "/",
+            sameSite: "none",
+            secure: false,
+         })
+         res.cookie("refreshToken", refreshToken, {
+            httpOnly: true,
+            path: "/",
+            sameSite: "none",
+            secure: false,
+          });
+          res.status(200).send({ accessToken: token, refreshToken });
+     } catch (error) {
+        next(error);
 
+     }
+ })
+
+ router.post("/signOut", authorize, async (req, res, next) => {
+    try {
+      req.user.refreshTokens = req.user.refreshTokens.filter(
+        (t) => t.token !== req.body.refreshToken
+      );
+      await req.user.save();
+      res.clearCookie("accessToken");
+      res.clearCookie("refreshToken");
+      res.send("You are succesfully Sign out from LinkedIn");
+    } catch (err) {
+      next(err);
+    }
+  });
+  
 router.post("/:id/upload", upload.single("image"), async (req, res, next) => {
     try {
         const imagesPath = path.join(__dirname, "./images");
