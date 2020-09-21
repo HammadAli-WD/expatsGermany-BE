85cf4becd0c5d7c569171fe2d17f52779544285c
Hammad-87 <pakswim@gmail.com>
2020-09-21 17:22:29 +0200

    OAuth with LikedIn and Facebook

--- services/user/index.js
+++ services/user/index.js
@@ -108,4 +108,120 @@ router.post("/:id/upload", upload.single("image"), async (req, res, next) => {
     res.send("Ok");
 
 })
+
+router.post("/refreshToken", async (req, res, next) => {
+  const oldRefreshToken = req.cookies.refreshToken;
+  if (!oldRefreshToken) {
+    const err = new Error("Refresh token missing");
+    err.httpStatusCode = 403;
+    next(err);
+  } else {
+    try {
+      const tokens = await refreshToken(oldRefreshToken);
+
+      res.cookie("accessToken", tokens.token, {
+        //httpOnly: true,
+        //path: "/",
+      });
+      res.cookie("refreshToken", tokens.refreshToken, {
+        //httpOnly: true,
+        path: ["/user/refreshToken", "/user/signOut"],
+      });
+      res.send(tokens);
+    } catch (error) {
+      console.log(error);
+      const err = new Error(error);
+      err.httpStatusCode = 403;
+      next(err);
+    }
+  }
+});
+
+// Modify a profile
+router.put("/", authorize, async (req, res, next) => {
+  try {
+    const profile = await UserModel.findOneAndUpdate(req.user._id, req.body);
+    if (profile) {
+      res.status(200).send("OK");
+    } else {
+      const error = new Error(`Profile with id ${req.params.id} not found!`);
+      error.httpStatusCode = 404;
+      next(error);
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
+// Delete a profile
+router.delete("/", async (req, res, next) => {
+  try {
+    const profile = await UserModel.findByIdAndDelete(req.user._id);
+    if (profile) {
+      res.status(200).send("Delete!");
+    } else {
+      const error = new Error(`Profile with id ${req.params.id} not found!`);
+      error.httpStatusCode = 404;
+      next(error);
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+router.get(
+  "/auth/fbSignIn",
+  passport.authenticate('facebook')
+)
+router.get(
+  "/auth/fbSignIn/redirect",
+  passport.authenticate("facebook"),
+  async (req, res, next) => {
+    try {
+      console.log(req.user)
+      const { token, refreshToken } = req.user.tokens
+      res.cookie("accessToken", token, {
+        httpOnly: true,
+        path: "/"
+      })
+      res.cookie("refreshToken", refreshToken, {
+        httpOnly: true,
+        path: ["/user/refreshToken", "/user/signOut"],
+      })
+      res.status(200).redirect("http://localhost:3000/feed")
+    } catch (error) {
+      console.log(error)
+      next(error)
+    
+    }
+  }
+)
+
+router.get(
+  "/auth/LinkedIn",
+  passport.authenticate('linkedin')
+)
+router.get(
+  "/auth/LinkedIn/redirect",
+  passport.authenticate("linkedin"),
+  async (req, res, next) => {
+    try {
+      console.log(req.user)
+      const { token, refreshToken } = req.user.tokens
+      res.cookie("accessToken", token, {
+        //httpOnly: true,
+        path: "/"
+      })
+      res.cookie("refreshToken", refreshToken, {
+        //httpOnly: true,
+        path: ["/user/refreshToken", "/user/signOut"],
+      })
+      res.status(200).redirect("http://localhost:3000/feed")
+    } catch (error) {
+      console.log(error)
+      next(error)
+    
+    }
+  }
+)
+
 module.exports = router
