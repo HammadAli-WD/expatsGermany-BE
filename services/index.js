1d2845a8557d2bb8e8cd2abb2a20ef3bbeeb92ae
Hammad Ali <63840111+HammadAli-WD@users.noreply.github.com>
2020-09-23 20:12:22 +0200

    update for passport initilization

--- services/index.js
+++ services/index.js
@@ -1,6 +1,9 @@
+require("dotenv").config();
 const router = require("express").Router();
 const userRouter = require("./user")
+const passport = require('passport');
 
+router.use(passport.initialize())
 router.use("/user", userRouter)
 
 module.exports = router
