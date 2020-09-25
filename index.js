a91dad9fd79a416642dca2abc43573422f8f074d
Hammad Ali <63840111+HammadAli-WD@users.noreply.github.com>
2020-09-25 23:02:36 +0200

    Updated

--- index.js
+++ index.js
@@ -1,26 +1,44 @@
 require("dotenv").config();
+const http = require("http")
 const express = require ("express");
-const startDB = require("./config/startDB");
-const services = require("./services");
-const app = new express();
-const morgan = require("morgan");
-const helmet = require("helmet");
 const cors = require("cors")
+const startDB = require("./config/startDB");
+const app = express();
+const authorize = require("./middlewares/authorize")
+const userRouter = require("./services/user")
+const passport = require('./utils/oauth');
+
+
+//const morgan = require("morgan");
+//const helmet = require("helmet");
 const cookieParser = require("cookie-parser");
 const listEndPoints = require("express-list-endpoints");
 const port = process.env.PORT
-app.use(cors())
+
+const whitelist = ["http://localhost:3000"];
+const corsOptions = {
+    origin: (origin, callback) => {
+      if (whitelist.indexOf(origin) !== -1 || !origin) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  };
 app.use(cookieParser())
+app.use(cors(corsOptions))
 app.use(express.json())
-app.use ("/api", services)
+app.use(passport.initialize())
+app.use("/user", userRouter)
 
 
-if(process.env.NODE_ENV !== "production"){
+/* if(process.env.NODE_ENV !== "production"){
     app.use(morgan("dev"))
 } else {
     app.use(helmet())
-}
-const server = require("http").createServer(app);
+} */
+const server = http.createServer(app);
 
 server.listen(port)
 server.on("listening", ()=> {
