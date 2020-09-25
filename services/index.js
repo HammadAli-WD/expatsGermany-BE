64ca136b8c3e0f88c6fdb812a4e8fb4825a47fac
Hammad Ali <63840111+HammadAli-WD@users.noreply.github.com>
2020-09-25 23:02:47 +0200

    Delete index.js

--- services/index.js
+++
@@ -1,9 +0,0 @@
-require("dotenv").config();
-const router = require("express").Router();
-const userRouter = require("./user")
-const passport = require('passport');
-
-router.use(passport.initialize())
-router.use("/user", userRouter)
-
-module.exports = router
