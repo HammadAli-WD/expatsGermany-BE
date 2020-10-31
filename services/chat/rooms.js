3a0c3c976d90e6b523e01c18e97594c5ed7d69ee
Hammad Ali <63840111+HammadAli-WD@users.noreply.github.com>
2020-10-31 13:28:01 +0100

    get route for rooms

--- services/chat/rooms.js
+++ services/chat/rooms.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const { createChatRoom } = require("../../utils/userRoom")
 const {authorize, adminOnly} = require('../../middlewares/authorize')
-
+const RoomModel = require('../../Models/ChatRooms')
 router.post('/room', authorize, adminOnly, async (req, res, next) => {    
     try {
        await createChatRoom(req, res)
@@ -9,6 +9,18 @@ router.post('/room', authorize, adminOnly, async (req, res, next) => {
       console.log(error);
     }
    })
+
+   router.get("/", authorize, async (req, res, next) => {
+    try{
+      //const ChatRoom = await RoomModel.findOne({ name })
+        const ChatRoom = await RoomModel.find();
+        res.status(200).send(ChatRoom);
+            
+    } catch (error) {
+      next(error)
+      console.log(error);
+    }
+})
 /* 
 router.get('/:room', (req, res) => {
   res.render('room', { roomId: req.params.room })
