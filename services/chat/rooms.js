3be00ecef57e7a7f8752131bd20e9825e71055a2
Hammad Ali <63840111+HammadAli-WD@users.noreply.github.com>
2020-11-15 03:20:12 +0100

    Update rooms.js

--- services/chat/rooms.js
+++ services/chat/rooms.js
@@ -1,25 +1,25 @@
 const router = require("express").Router();
 const { createChatRoom } = require("../../utils/userRoom")
-const {authorize, adminOnly} = require('../../middlewares/authorize')
+const { authorize, adminOnly } = require('../../middlewares/authorize')
 const RoomModel = require('../../Models/ChatRooms')
-router.post('/room', authorize, adminOnly, async (req, res, next) => {    
-    try {
-       await createChatRoom(req, res)
-    } catch (error) {      
-      console.log(error);
-    }
-   })
+router.post('/room', authorize, adminOnly, async (req, res, next) => {
+  try {
+    await createChatRoom(req, res)
+  } catch (error) {
+    console.log(error);
+  }
+})
 
-   router.get("/", authorize, async (req, res, next) => {
-    try{
-      //const ChatRoom = await RoomModel.findOne({ name })
-        const ChatRoom = await RoomModel.find();
-        res.status(200).send(ChatRoom);
-            
-    } catch (error) {
-      next(error)
-      console.log(error);
-    }
+router.get("/", async (req, res, next) => {
+  try {
+    //const ChatRoom = await RoomModel.findOne({ name })
+    const ChatRoom = await RoomModel.find();
+    res.status(200).send(ChatRoom);
+
+  } catch (error) {
+    next(error)
+    console.log(error);
+  }
 })
 /* 
 router.get('/:room', (req, res) => {
