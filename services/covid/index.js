08352b6a10f6e29d79f70550eeb341833b5e9d7b
Hammad Ali <63840111+HammadAli-WD@users.noreply.github.com>
2022-02-20 23:07:14 +0100

    Update index.js

--- services/covid/index.js
+++ services/covid/index.js
@@ -10,7 +10,7 @@ router.get('/germanyCases', (req, res, next) =>{
         fetch(url + '/summary')
          .then(res => res.json())
          .then(data => 
-            res.send(data.Countries[64]))
+            res.send(data.Countries[65]))
     } catch (error) {
       next(error)
       console.log(error);
